a949c15c4c9b47cc04af8f959c9140b69d10eb86
Karsenh <KarsenHansen@gmail.com>
2021-07-08 03:38:27 -0700

    initial redux toolkit demo commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './redux/counter';
+
 function App() {
+  const count = useSelector(state => state.counter.count);
+  const [inputValue, setInputValue] = useState(1);
+  const dispatch = useDispatch();
+
   return (
     <div className='App'>
-      <h1>Welcome to your 'custom-cra-template' App!</h1>
+      <h1>Redux Toolkit Demo</h1>
+      <p>Value: {count} </p>
+      <input
+        value={inputValue}
+        onChange={e => setInputValue(parseInt(e.target.value))}
+      />
+      <br />
+      <br />
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button
+        onClick={() => {
+          console.log(inputValue);
+          dispatch(incrementByAmount(inputValue));
+        }}
+      >
+        Add Custom Amount
+      </button>
     </div>
   );
 }
